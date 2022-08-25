6412255824d8a6548ef6e450ca38fd74335d5d1c
Zancanaro93 <marco.zancanaro@yahoo.com.br>
2022-08-25 00:45:44 -0300

    Commit Desafio pratica-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,4 @@ let seuEmail = "marco.zancanaro@yahoo.com.br"
 let suaProfissao = "Advogado"
 let suaIdade = "29"
 let oLugarQueVoceSonhaUmDiaConhecer = "Roma"
+console.log (`Olá, ${seuNome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${oLugarQueVoceSonhaUmDiaConhecer}. Confirma pra gente alguns dados? A sua idade é ${suaIdade}, você é ${suaProfissao} e o seu email é ${seuEmail}? Ficamos no aguardo. Muito obrigado e boa viagem! :D)`)
