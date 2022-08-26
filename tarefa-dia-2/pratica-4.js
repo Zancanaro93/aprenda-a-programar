bf22f4b9c6ba3e554feb9e70de0f1f1a8d9ea725
Zancanaro93 <marco.zancanaro@yahoo.com.br>
2022-08-26 18:16:36 -0300

    commit tarefa 3

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,10 @@
+let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
+var "internacional"
+
+if ("internacional" = true) 
+{ ("valor" *1.2)
+} else ("internacional" false) {
+    console.log ("valor" *1.12)
+}
